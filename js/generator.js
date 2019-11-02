8dd9ffde214687005f25edfbca2963624b27b404
Raziel M. Rodrigues <raziel.rodrigues@fatec.sp.gov.br>
2019-11-02 17:33:39 +0000

    arrumando e otimizando codigo

--- js/generator.js
+++ js/generator.js
@@ -1,7 +1,10 @@
 generateLink.addEventListener('submit', function(e) {
-	var phoneNumber = document.getElementById('phoneNumber');
-	var userMessage = document.getElementById('userMessage');
-	var link = 'https://api.whatsapp.com/send?phone='+phoneNumber+'&text='+userMessage;
-	document.getElementById('generatedLink').innerHTML = link;
+	var phoneNumber = document.getElementById('phoneNumber').value;
+	var userMessage = document.getElementById('userMessage').value;
+	var generatedLink = 'https://api.whatsapp.com/send?phone='+phoneNumber+'&text='+userMessage;
+	var link = document.getElementById('generatedLink');
+	link.innerHTML = generatedLink;
+	link.setAttribute('href', generatedLink);
+	link.setAttribute('href', generatedLink);
 e.preventDefault();
 });
