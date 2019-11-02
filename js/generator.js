4391a31c6278744d01acc81a7a24621aa75642e8
Raziel M. Rodrigues <raziel.rodrigues@fatec.sp.gov.br>
2019-11-02 19:17:46 +0000

    terminando layout final

--- js/generator.js
+++ js/generator.js
@@ -2,7 +2,7 @@ generateLink.addEventListener('submit', function(e) {
 	var phoneNumber = document.getElementById('phoneNumber').value;
 	var userMessage = document.getElementById('userMessage').value;
 	var hide = document.getElementById('hide');
-	var generatedLink = 'https://api.whatsapp.com/send?phone='+phoneNumber+'&text='+userMessage;
+	var generatedLink = 'https://api.whatsapp.com/send?phone=+'+phoneNumber+'&text='+userMessage;
 	var link = document.getElementById('generatedLink');
 	link.innerHTML = generatedLink;
 	link.setAttribute('href', generatedLink);
