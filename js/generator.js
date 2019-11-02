3a470aeb349968b819c4857f47eabb4a37978cd0
Raziel M. Rodrigues <raziel.rodrigues@fatec.sp.gov.br>
2019-11-02 18:30:59 +0000

    quase chegando no final
    Criei duas funções novas para poder esconder a div onde se encontra o link gerado e estou melhorando o visual da aplicação tal como melhorando o código

--- js/generator.js
+++ js/generator.js
@@ -1,10 +1,13 @@
 generateLink.addEventListener('submit', function(e) {
 	var phoneNumber = document.getElementById('phoneNumber').value;
 	var userMessage = document.getElementById('userMessage').value;
+	var hide = document.getElementById('hide');
 	var generatedLink = 'https://api.whatsapp.com/send?phone='+phoneNumber+'&text='+userMessage;
 	var link = document.getElementById('generatedLink');
 	link.innerHTML = generatedLink;
 	link.setAttribute('href', generatedLink);
-	link.setAttribute('href', generatedLink);
+	if (hide.className == 'hide') {
+		hide.className = 'show';
+	}
 e.preventDefault();
 });
